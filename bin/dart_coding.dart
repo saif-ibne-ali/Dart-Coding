2d9549e7cde50c3dd419666cf5cf881ab5476aa6
saif-ibne-ali <si87868@gmail.com>
2023-10-04 22:11:50 +0600

    Dart Coding

---
+++ bin/dart_coding.dart
@@ -0,0 +1,5 @@
+import 'package:dart_coding/dart_coding.dart' as dart_coding;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_coding.calculate()}!');
+}
---
+++ test/dart_coding_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_coding/dart_coding.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
